fb670a53c28740ed6186cb2de8a8623c595bb053
rishsheth <91674813+TheGrisleyBear123@users.noreply.github.com>
2024-12-21 18:47:35 +0000

    the image links to a now the github repo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => { 
     const image = document.getElementById("whackImage"); 
     image.addEventListener('click', () => {
-        window.open('https://github.com/TheGrisleyBear123/WhackAmole/archive/refs/heads/master.zip', '_blank');
+        window.open('https://github.com/TheGrisleyBear123/WhackAmole', '_blank');
     }); 
 }); 
