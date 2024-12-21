59e1837f8c96f3e209e84364d7e76159f4359a27
rishsheth <91674813+TheGrisleyBear123@users.noreply.github.com>
2024-12-21 18:02:54 +0000

    Made fixes to my html code

---
+++ index.js
@@ -0,0 +1,4 @@
+const image = document.getElementById("whack"); 
+image.addEventListener('click', () => {
+    window.open('https://github.com/TheGrisleyBear123/WhackAmole/archive/refs/heads/master.zip', '_blank'); 
+}); 
