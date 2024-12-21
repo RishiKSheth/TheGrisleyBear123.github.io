6960a66ed13d9391c7da3e9b4300a6c275d6fc3b
rishsheth <91674813+TheGrisleyBear123@users.noreply.github.com>
2024-12-21 18:09:59 +0000

    the image finally links to a file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const image = document.getElementById("whack"); 
-image.addEventListener('click', () => {
-    window.open('https://github.com/TheGrisleyBear123/WhackAmole/archive/refs/heads/master.zip', '_blank'); 
+document.addEventListener('DOMContentLoaded', () => { 
+    const image = document.getElementById("whackImage"); 
+    image.addEventListener('click', () => {
+        window.open('https://github.com/TheGrisleyBear123/WhackAmole/archive/refs/heads/master.zip', '_blank');
+    }); 
 }); 
