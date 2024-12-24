ff326d1404ebb5f20ff1cbd6436319f1e2757509
rishsheth <91674813+TheGrisleyBear123@users.noreply.github.com>
2024-12-24 03:42:11 +0000

    added icon for website in tab

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => { 
+    /*for whack a mole project*/
     const image = document.getElementById("whackImage");
     image.addEventListener('click', () => {
         window.open('https://github.com/TheGrisleyBear123/WhackAmole', '_blank');
     }); 
-
+    
+    /*for animations */
     image.addEventListener('mouseenter', () => {
         image.classList.add('zoom-animation'); 
     });
@@ -12,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         image.classList.remove('zoom-animation'); 
     }); 
 
+    /* for custom cursor */
     document.body.addEventListener('mousedown', () => { 
         document.body.style.cursor = "url('images/clickcustomcursor.png'), auto";
     }); 
